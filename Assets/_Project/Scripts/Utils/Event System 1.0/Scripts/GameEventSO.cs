d6e5ae7ff4cd5531ba48614a664b601f38494732
QuestioningCat <cm66.work@gmail.com>
2023-03-15 23:43:58 +0400

    Added Logging to GameEventSO
    Added the ability to tell GameEventSO to Raise log events when event listeners are Registered, Unregistered and Raised.
    This is building upon my work on my Open Event System.

--- Assets/_Project/Scripts/Utils/Event System 1.0/Scripts/GameEventSO.cs
+++ Assets/_Project/Scripts/Utils/Event System 1.0/Scripts/GameEventSO.cs
@@ -10,25 +10,37 @@ namespace Utils.Events
     {
         public List<GameEventListener> Listeners = new List<GameEventListener>();
 
+        [Header("Logging")]
+        public GameEventSO LoggerEventSO;
+
         // Raise event though different methods signatures.
-        public void Raise(Component sender, object data)
+        public void Raise(Component sender, object data, bool generateLogEvent = false)
         {
             for(int i = 0; i < Listeners.Count; i++)
             {
                 Listeners[i].OnEventRaised(sender, data);
+                if(generateLogEvent && LoggerEventSO != null)
+                    LoggerEventSO.Raise(null, $"{this.name} Raised to {Listeners[i].gameObject.name}");
             }
         }
         // Manage Listeners
 
-        public void RegisterListener(GameEventListener listener)
+        public void RegisterListener(GameEventListener listener, bool generateLogEvent = false)
         {
             if(!Listeners.Contains(listener))
                 Listeners.Add(listener);
+
+            if(generateLogEvent && LoggerEventSO != null)
+                LoggerEventSO.Raise(null, $"Registered: {listener.gameObject.name} to {this.name}");
         }
-        public void UnregisterListener(GameEventListener listener)
+        public void UnregisterListener(GameEventListener listener, bool generateLogEvent = false)
         {
             if(Listeners.Contains(listener))
                 Listeners.Remove(listener);
+
+
+            if(generateLogEvent && LoggerEventSO != null)
+                LoggerEventSO.Raise(null, $"Unregistered: {listener.gameObject.name} to {this.name}");
         }
     }
 }
